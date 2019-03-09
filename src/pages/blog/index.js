fdc940d05c634388c49efa82b5e8cfbb39750e91
Steven Akins <stevena@purecars.com>
2019-03-09 12:38:31 -0500

    remove boilerplate

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -26,7 +26,7 @@ export default class BlogIndexPage extends React.Component {
                     padding: '1rem',
                   }}
                 >
-                  Latest Stories
+                  Latest Projects
                 </h1>
               </div>
             </div>
